bca52e185caba89691b67e96a19fdf4f801dd5ca
iChetan7 <110901229+iChetan7@users.noreply.github.com>
2025-07-19 14:06:21 +0530

    Update submit.js

--- api/submit.js
+++ api/submit.js
@@ -1,1 +1,22 @@
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const formData = new URLSearchParams(req.body).toString();
+
+    const response = await fetch("https://formsubmit.co/infinitycsgamer@gmail.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: formData
+    });
+
+    if (response.ok) {
+      res.redirect(302, "/thanks.html");
+    } else {
+      res.status(500).send("Failed to submit form");
+    }
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+}
 
